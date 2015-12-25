8a289df7b9572e6c0b33b48d910be535b0ba6fa8
lili <lzhang0409@gmail.com>
2015-12-25 11:09:53 +0800

    make the program to satisfy multiple users' needs

--- stock_price_prediction.py
+++ stock_price_prediction.py
@@ -1,17 +1,13 @@
 """
-What have been done:
-1. scrape all historical data on the SP500 from Yahoo Finance back to 1950 by keeping going to "next" link
+1. scrape historical data of the date range that the user specifies on the SP500 from Yahoo Finance back by keeping going to "next" link
 2. write them into a single csv file
 3. read csv data
 4. calculate indicators used for modelings
 5. remove rows with "NaN"
 6. splitting data into train data and test data
-7. make predictions in the backtesting way with linear regression as the initial modeling method
+7. make predictions for the prediction period that the user specifies in the backtesting way with linear regression as the initial modeling method
 8. select mae as the error measurement and calculate it
 9. write predicted data to csv file
-To do:
-1. optimize the code and make it an automatic system that can be used everyday
-2. try other modeling methods like random forest to improve the prediction accurary
 """
 
 import requests
@@ -20,106 +16,129 @@ import csv
 import time
 import pandas as pd
 from datetime import datetime
+from datetime import timedelta
 from sklearn.linear_model import LinearRegression
+import sys
 
-def getData(pageUrl):
+def getQueryInput():
+    print 'Please provide the start date and end date of historical data you want to retrieve.'
+    start_date = raw_input('Please enter the query start date in format YYYY-MM-DD(i.e. 2015-12-21):')
+    end_date = raw_input('Please enter the query end date in format YYYY-MM-DD(i.e. 2015-12-21):')
+    output_file = raw_input('Please enter the csv file name you want to load data to (i.e. acd.csv):')
+    a = int(start_date.split('-')[1])-1
+    b = int(start_date.split('-')[2])
+    c = int(start_date.split('-')[0])
+    d = int(end_date.split('-')[1])-1
+    e = int(end_date.split('-')[2])
+    f = int(end_date.split('-')[0])
+    global first_link
+    first_link = '/q/hp?s=%5EGSPC&' + 'a=%s&b=%s&c=%s&d=%s&e=%s&f=%s&g=d' % (a,b,c,d,e,f)
+    start_date = time.strptime(start_date, '%Y-%m-%d')
+    end_date = time.strptime(end_date, '%Y-%m-%d')
+    if start_date <= end_date:
+        return output_file, first_link
+    else:
+        sys.exit('Start date should be earlier than end date. Review your inputs and re-execute the program.')
+
+def getData(output_file, pageUrl):
     try:
-        response = requests.get("https://finance.yahoo.com"+pageUrl)
+        response = requests.get('https://finance.yahoo.com'+pageUrl)
     except HTTPError as e:
         print e
     content = response.content
-    parser = BeautifulSoup(content, "html.parser")
+    parser = BeautifulSoup(content, 'html.parser')
 
-    with open("stock.csv", "a") as f:
+    with open(output_file, 'a') as f:
         writer = csv.writer(f)
         try:
-            table = parser.find("table", class_="yfnc_datamodoutline1")
+            table = parser.select('.yfnc_datamodoutline1')[0]
         except:
-            print 'Table cannot be found in this link. This may be caused by network problem. Try later.'
-            print "https://finance.yahoo.com"+pageUrl
-        try:
-            table = table.find("table")
-        except:
-            print 'Nested table cannot be found in this link. This may be caused by network problem. Try later.'
-            print "https://finance.yahoo.com"+pageUrl
-        if pageUrl == "/q/hp?s=%5EGSPC+Historical+Prices":
-            tr = table.find('tr')
-            td = tr.find_all('th', class_="yfnc_tablehead1")
-            row = [element.text.strip().strip("*").encode('utf-8') for element in td]
+            print 'Table cannot be found in the link https://finance.yahoo.com%s. This may be caused by network problem. Try later.' % pageUrl
+        if pageUrl == first_link:
+            table_heads = table.select('th')
+            row = [table_head.text.strip().strip("*").encode('utf-8') for table_head in table_heads]
             writer.writerow(row)
-        for tr in table.find_all("tr")[2:]:
-            td = tr.find_all("td")
-            if len(td) > 1:
-                row = []
-                for i, element in enumerate(td):
-                    if i == 0:
-                        element = element.text.strip().encode('utf-8')
-                        if "," in element:
-                            element = time.strptime(element, "%b %d, %Y")
-                            element = time.strftime("%Y-%m-%d", element)
-                        else:
-                            element = time.strptime(element, "%Y-%m-%d")
-                            element = time.strftime("%Y-%m-%d", element)
-                        row.append(element)
+        table_rows = table.select('tr')[1:]
+        for table_row in table_rows[1:-1]:
+            row = []
+            for index, table_data in enumerate(table_row):
+                if index == 0:
+                    table_data = table_data.text.strip().encode('utf-8')
+                    if ',' in table_data:
+                        table_data = time.strptime(table_data, '%b %d, %Y')
+                        table_data = time.strftime('%Y-%m-%d', table_data)
                     else:
-                        element = element.text.strip().replace(",", "").encode("utf-8")
-                        row.append(element)
-                writer.writerow(row)
-    new = parser.find("a", rel="next")
-    if new != None:
-        newPage = new.attrs["href"]
-        getData(newPage)
+                        table_data = time.strptime(table_data, '%Y-%m-%d')
+                        table_data = time.strftime('%Y-%m-%d', table_data)
+                    row.append(table_data)
+                else:
+                    table_data = table_data.text.strip().replace(',', '').encode('utf-8')
+                    row.append(table_data)
+            writer.writerow(row)
+    new_page = parser.find('a', rel='next')
+    if new_page != None:
+        new_page_link = new_page.attrs['href']
+        getData(output_file, new_page_link)
     else:
         return
 
-getData("/q/hp?s=%5EGSPC+Historical+Prices")
+def getPredectionReq():
+    print 'Please provide the start date and end date of the prediction period you want.'
+    prediction_start_date = raw_input('Pleae enter the prediction start date in format YYYY-MM-DD(i.e. 2015-12-21):')
+    prediction_end_date = raw_input('Pleae enter the prediction end date in format YYYY-MM-DD(i.e. 2015-12-21):')
+    predicted_output_file = raw_input('Please enter the csv file name you want to save predicted results to (i.e. acd.csv):')
+    prediction_start_date = datetime(int(prediction_start_date.split('-')[0]), int(prediction_start_date.split('-')[1]), int(prediction_start_date.split('-')[2]))
+    prediction_end_date = datetime(int(prediction_end_date.split('-')[0]), int(prediction_end_date.split('-')[1]), int(prediction_end_date.split('-')[2]))
+    if prediction_start_date <= prediction_end_date:
+        return predicted_output_file, prediction_start_date, prediction_end_date
+    else:
+        sys.exit('Start date should be earlier than end date. Review your inputs and re-execute the program.')
 
 
-df = pd.read_csv('stock.csv')
-df['Date'] =  pd.to_datetime(df['Date'], format='%Y-%m-%d')
-df = df.sort(['Date'], ascending=[True])
+def predict(output_file, predicted_output_file, prediction_start_date, prediction_end_date):
+    df = pd.read_csv(output_file)
+    df['Date'] =  pd.to_datetime(df['Date'], format='%Y-%m-%d')
+    df['year'] = pd.DatetimeIndex(df['Date']).year
+    df = df.set_index('Date', drop=True)
+    df = df.sort_index(axis=0, ascending=True)
+    df['avg_close_price_day_5'] = pd.rolling_mean(df['Close'], window=5).shift(1)
+    df['avg_close_price_day_30'] = pd.rolling_mean(df['Close'], window=30).shift(1)
+    df['avg_close_price_day_365'] = pd.rolling_mean(df['Close'], window=365).shift(1)
+    df['ratio_avg_close_price_5_365'] = df['avg_close_price_day_5'] / df['avg_close_price_day_365']
+    df['std_close_price_day_5'] = pd.rolling_std(df['Close'], window=5).shift(1)
+    df['std_close_price_day_365'] = pd.rolling_std(df['Close'], window=365).shift(1)
+    df['ratio_std_close_price_5_365'] = df['std_close_price_day_5'] / df['std_close_price_day_365']
+    df['avg_volume_day_5'] = pd.rolling_mean(df['Volume'], window=5).shift(1)
+    df['avg_volume_day_365'] = pd.rolling_mean(df['Volume'], window=365).shift(1)
+    df['ratio_volume_5_365'] = df['avg_volume_day_5'] / df['avg_volume_day_365']
+    df['std_avg_volume_5'] = pd.rolling_mean(df['avg_volume_day_5'], window=5).shift(1)
+    df['std_avg_volume_365'] = pd.rolling_mean(df['avg_volume_day_365'], window=365).shift(1)
+    df['ratio_std_avg_volume_5_365'] = df['std_avg_volume_5'] / df['std_avg_volume_365']
 
-df['avg_close_price_day_5'] = pd.rolling_mean(df['Close'], window=5).shift(1)
-df['avg_close_price_day_30'] = pd.rolling_mean(df['Close'], window=30).shift(1)
-df['avg_close_price_day_365'] = pd.rolling_mean(df['Close'], window=365).shift(1)
-df['ratio_avg_close_price_5_365'] = df['avg_close_price_day_5'] / df['avg_close_price_day_365']
-df['std_close_price_day_5'] = pd.rolling_std(df['Close'], window=5).shift(1)
-df['std_close_price_day_365'] = pd.rolling_std(df['Close'], window=365).shift(1)
-df['ratio_std_close_price_5_365'] = df['std_close_price_day_5'] / df['std_close_price_day_365']
-df['avg_volume_day_5'] = pd.rolling_mean(df['Volume'], window=5).shift(1)
-df['avg_volume_day_365'] = pd.rolling_mean(df['Volume'], window=365).shift(1)
-df['ratio_volume_5_365'] = df['avg_volume_day_5'] / df['avg_volume_day_365']
-df['std_avg_volume_5'] = pd.rolling_mean(df['avg_volume_day_5'], window=5).shift(1)
-df['std_avg_volume_365'] = pd.rolling_mean(df['avg_volume_day_365'], window=365).shift(1)
-df['ratio_std_avg_volume_5_365'] = df['std_avg_volume_5'] / df['std_avg_volume_365']
-df['year'] = pd.DatetimeIndex(df['Date']).year
+    df = df[['Close'] + list(df.columns[6:])]
 
-df = df[['Date', 'Close'] + list(df.columns[7:])]
+    df = df.dropna(axis=0)
 
-df = df[df['Date'] > datetime(year=1951, month=1, day=2)]
-df = df.dropna(axis=0)
-df = df.reset_index(drop=True)
+    predicted_values = []
+    df_prediction = pd.DataFrame()
+    df_prediction['Actual'] = df.ix[prediction_start_date:prediction_end_date]['Close']
 
-total_rows = df['Date'].count()
-threshold = df[df["Date"] == datetime(year=2015, month=12, day=2)].index.tolist()[0]
-predicted_values = []
+    regressor = LinearRegression()
+    for index in df_prediction.index:
+        train = df.ix[df.index[0]: index-timedelta(days=1)]
+        test = df.ix[index:index]
+        train_predictors = train[list(df.columns[1:])]
+        train_to_predict = train['Close']
+        regressor.fit(train_predictors, train_to_predict)
+        test_predictors = test[list(df.columns[1:])]
+        predicted_values.append(regressor.predict(test_predictors)[0])
+    df_prediction['Predicted'] = predicted_values
+    df_prediction.to_csv(predicted_output_file, index=True)
+    mae = sum(abs(df_prediction['Actual'] - df_prediction['Predicted'])) / len(df_prediction['Predicted'])
+    print 'The mean absolute error is', mae
 
-regressor = LinearRegression()
-
-for row in range(threshold, total_rows):
-    train = df.iloc[0:row]
-    test = df.iloc[row]
-    train_predictors = train[list(df.columns[2:])]
-    train_to_predict = train['Close']
-    regressor.fit(train_predictors, train_to_predict)
-    test_predictors = test[list(df.columns[2:])]
-    predicted_values.append(regressor.predict(test_predictors)[0])
-
-result = pd.DataFrame()
-result['Date'] = df['Date'].iloc[threshold:total_rows]
-result['Actual'] = df['Close'].iloc[threshold:total_rows]
-result['Predicted'] = predicted_values
-result.to_csv('a_predicted_result.csv', index=False)
-
-mae = sum(abs(result['Actual'] - result['Predicted'])) / len(result['Predicted'])
-print 'The mean absolute error is', mae
+if __name__ == '__main__':
+    output_file, first_link = getQueryInput()
+    getData(output_file, first_link)
+    predicted_output_file, prediction_start_date, prediction_end_date = getPredectionReq()
+    predict(output_file, predicted_output_file, prediction_start_date, prediction_end_date)
