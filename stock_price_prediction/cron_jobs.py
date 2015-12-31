fd8c7198881027dfa99733a71e721947c3b3d306
lili <lzhang0409@gmail.com>
2015-12-31 21:19:53 +0800

    read cron job command from settings.py

--- stock_price_prediction/cron_jobs.py
+++ stock_price_prediction/cron_jobs.py
@@ -1,8 +1,9 @@
 from crontab import CronTab
+import settings as setting
 
 cron = CronTab()
-job_data = cron.new(command='/Users/lilizhang/anaconda/bin/python /Users/lilizhang/documents/data_science_project_code/stock_price_data_acquisition.py')
+job_data = cron.new(command = setting.CRON_DATA_JOB_COMMAND)
 job_data.setall('0 23 * * *')
-job_prediction = cron.new(command='/Users/lilizhang/anaconda/bin/python /Users/lilizhang/documents/data_science_project_code/stock_price_prediction.py')
+job_prediction = cron.new(command = setting.CRON_PREDICTION_JOB_COMMAND)
 job_prediction.setall('0 1 * * *')
 cron.write()
