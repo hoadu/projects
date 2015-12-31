1af5b6a06cacbb1ed40cb91defd2a841e86fabd6
lili <lzhang0409@gmail.com>
2015-12-31 21:20:38 +0800

    add some comments

--- stock_price_prediction/settings.py
+++ stock_price_prediction/settings.py
@@ -3,6 +3,7 @@ import datetime
 QUERY_START_DATE = '1950-01-01'
 QUERY_END_DATE = '2015-12-31'
 
+#change QUERY_START_DATE and QUERY_END_DATE to the below after first-time operation to automate daily data acquisition 
 #QUERY_START_DATE = str(datetime.date.today())
 #QUERY_END_DATE = str(datetime.date.today())
 
@@ -12,9 +13,16 @@ PREDICT_TOMMORROW = str(datetime.date.today() + datetime.timedelta(days=1))
 
 DATA_STORAGE_METHOD = 'mysql'
 
-DATABASE_CONNECTION = 'mysql+mysqlconnector://thrifty:123456@localhost/thrifty'
+#customize database_connection with your database API and access info
+#example: DATABASE_CONNECTION = 'mysql+mysqlconnector://thrifty:123456@localhost/thrifty'
+DATABASE_CONNECTION = 'mysql+mysqlconnector://DB_USER:DB_USER_PASS@HOST_IP/DB_NAME'
 HISTORICAL_DATA_TABLE = 'historical_data'
 PREDICTED_OUTPUT_TABLE = 'predicted_output'
 
 HISTORICAL_DATA_FILE = 'historical_data.csv'
 PREDICTED_OUTPUT_FILE = 'predicted_output.csv'
+
+#customize cron job command
+#example: CRON_DATA_JOB_COMMAND = '/Users/lilizhang/anaconda/bin/python /Users/lilizhang/documents/data_science_project_code/stock_price_data_acquisition.py'
+CRON_DATA_JOB_COMMAND = 'PYTHON_PATH DATA_ACQUISITION_PYTHON_FILE_PATH'
+CRON_PREDICTION_JOB_COMMAND = 'PYTHON_PATH PREDICTION_PYTHON_FILE_PATH'
